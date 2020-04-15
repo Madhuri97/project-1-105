bff95b7045f00d7eb1ff5bb7c341b8b6e6882b3a
Madhuri <madhiurinaidu1997@msitprogram.net>
2020-04-15 23:00:01 +0530

    Handeled server side requests with flask

--- index.py
+++ index.py
@@ -1,11 +1,18 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
 def defaultPage():
     return '<h1>Search for Page after /</h1>'
 
-@app.route("/register")
+@app.route("/register", methods = ['POST', 'GET'])
 def result():
+    if request.method == 'POST':
+        output = request.form
+        return render_template("result.html", result = output)
+    else:
         return render_template("Registration.html")
 
+if __name__ == '__main__':
+    app.run(debug = True)
+    
