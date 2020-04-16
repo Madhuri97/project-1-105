f70a37964c8f94a8397f0c658d7ee62cce54b219
Madhuri <madhiurinaidu1997@msitprogram.net>
2020-04-16 13:25:32 +0530

    Updated index.py according to review

--- index.py
+++ index.py
@@ -2,14 +2,12 @@ from flask import Flask, redirect, url_for, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
-def defaultPage():
-    return '<h1>Search for Page after /</h1>'
-
 @app.route("/register", methods = ['POST', 'GET'])
 def result():
     if request.method == 'POST':
         output = request.form
-        return render_template("result.html", result = output)
+        print(output['name'])
+        return render_template("result.html", result = output)   
     else:
         return render_template("Registration.html")
 
