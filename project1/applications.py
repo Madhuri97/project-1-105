d13cd66efd2c7528028db5eb4959b45c04b12796
Madhuri <madhiurinaidu1997@msitprogram.net>
2020-04-20 22:37:26 +0530

    Allowed authenticated user to home and logout

--- project1/applications.py
+++ project1/applications.py
@@ -1,5 +1,4 @@
 import os
-
 from flask import Flask, session
 from flask_session import Session
 from sqlalchemy import create_engine, desc
@@ -9,11 +8,25 @@ import logging
 from Schema import *
 
 logging.basicConfig(level=logging.DEBUG)
+
 app = Flask(__name__)
 
 # Check for environment variable
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+db.init_app(app)
+
+# Set up database
+engine = create_engine(os.getenv("DATABASE_URL"))
+# Session = scoped_session(sessionmaker(bind=engine))
+# session = Session()
 
 #set up routes
 @app.route("/")
@@ -44,8 +57,11 @@ def login():
     user = schema.query.filter_by(email = request.form['email']).first()
     if user is not None:
         if  request.form["pswd"] == user.pwd:
+            session['email'] = request.form['email']
+            print(session)
             return redirect('/home')
         else:
+            print("Wrong credentials")
             var = "Wrong Cresdentials" 
             return render_template("Registration.html", message1 = var)
     else:
@@ -55,9 +71,20 @@ def login():
 
 @app.route("/home")
 def home():
+    try: 
+        user = session['email']
+        return render_template("login.html")
+    except:
+        var = "You must login to view the homepage"
         return render_template("Registration.html", message1 = var)
 
 @app.route("/logout")
 def logout():
+    try: 
+        user = session['email']
+        session.clear()
         var = "Logged Out"
+        return render_template("Registration.html", message1 = var)
+    except:
+        var = "You must logout from the page"
         return render_template("Registration.html", message1 = var)
