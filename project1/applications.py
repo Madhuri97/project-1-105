6ea90cdef8bb712306663833f84090656e5a6f4c
Madhuri <madhiurinaidu1997@msitprogram.net>
2020-04-28 12:06:39 +0530

    Created a route in home for search page

--- project1/applications.py
+++ project1/applications.py
@@ -1,19 +1,20 @@
 import os
-from flask import Flask, session
+from flask import Flask, session, redirect
 from flask_session import Session
-from sqlalchemy import create_engine, desc
+from sqlalchemy import create_engine, desc, or_
 from sqlalchemy.orm import scoped_session, sessionmaker
-from flask import Flask, redirect, url_for, render_template, request, session
+from flask import Flask, url_for, render_template, request, session
 import logging
 from Schema import *
+from books_db import Books
 
 logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
 
 # Check for environment variable
-if not os.getenv("DATABASE_URL"):
-    raise RuntimeError("DATABASE_URL is not set")
+# if not os.getenv("DATABASE_URL"):
+#     raise RuntimeError("DATABASE_URL is not set")
 
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -34,13 +35,13 @@ engine = create_engine(os.getenv("DATABASE_URL"))
 def result():
     db.create_all()
     if request.method == 'POST':
-        udata = schema(request.form["name"], request.form["email"], request.form["pswd"])
+        userdata = schema(request.form["name"], request.form["email"], request.form["pswd"])
         user = schema.query.filter_by(email = request.form['email']).first()
         if user is not None:
             print("User already exists. try to register if you are new user")
             var = "User already exists. please try to register if you are a new user"
             return render_template("Registration.html", message1 = var)
-        db.session.add(udata)
+        db.session.add(userdata)
         db.session.commit()
         var = 'Registration is successful'
         return render_template("Registration.html", message = var)   
@@ -69,12 +70,22 @@ def login():
         var = "You are not a registered user, Please first register to login"
         return render_template("Registration.html", message1 = var)
 
-@app.route("/home")
+@app.route("/home", methods = ['POST', 'GET'])
 def home():
     try: 
         user = session['email']
-        return render_template("login.html")
-    except:
+        if request.method == 'POST':
+            req = request.form['Search']
+            print(req)
+            reqs = str(req)
+            booksdata = db.session.query(Books.isbn, Books.title, Books.author, Books.year).filter(or_(Books.title.like("%"+reqs+"%"), Books.author.like("%"+reqs+"%"), Books.isbn.like("%"+reqs+"%"))).all()
+            if booksdata.__len__() == 0:
+                var = "No search found!"
+                return render_template("login.html", user = user, message1 = var)
+            return render_template("login.html", booksdata = booksdata, formaction = '/home', user = user)  
+        return render_template("login.html", user =user)
+    except Exception as e:
+        print(e)
         var = "You must login to view the homepage"
         return render_template("Registration.html", message1 = var)
 
